61e240fd4f491d0da0a8ef01735eaadf8cf250cd
Lagro222 <wbenkhaira@gmail.com>
2026-05-24 09:29:44 +0000

    changing: kernel.asm loads kernil.c

--- src/kernel/kernel.asm
+++ src/kernel/kernel.asm
@@ -1,56 +1,7 @@
 org 0x8000
 
-bits 16
-
-%define ENDL 0x0D,0x0A
+bits 32
 
 start:
-
-    cli
-
-    mov ax, 0x0000
-    mov ds, ax
-    mov es, ax
-
-    mov ax ,0x9000
-    mov ss, ax
-    mov sp,0xFFFF
-
-    sti
-
-
-    mov si, msg
-    call print_string
-
-    mov al, 0x01
-    mov cx, 0x0607
-    int 0x10
-
- command_loop:
-    mov si, prompt
-
-
-
-
-
-
-hang:
-
-    hlt
-    jmp hang
-
-
-print_string:
-    lodsb
-    cmp al, 0
-    je .done
-    mov ah, 0x0E
-    int 0x10
-    jmp print_string
-
-.done:
-    ret
-
-msg  db "kernel working",ENDL , 0
-msg_2 db "tedting" ,0
-
+  call kmain
+  jmp $   
