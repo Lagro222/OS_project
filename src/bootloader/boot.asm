df91e6ba772d76c93a3ded35e1f56f5df303276d
Lagro222 <wbenkhaira@gmail.com>
2026-05-26 10:24:00 +0000

    changes: using const to test

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -3,125 +3,138 @@ org 0x7C00
 
 %define ENDL 0x0D,0x0A
 
-start:
+CODE_SEG equ 0x08
+DATA_SEG equ 0x10
 
+BOOT_DRIVER db 0
+start:
+    mov [BOOT_DRIVER], dl
+
+    ;clear screen
     mov ax,0x0003
     int 0x10
-   ; stop intureping
-    cli
-
-    mov ax,0x9000
-    mov ss, ax
-    mov sp,0xFFFF
-
-    sti  ;interuping back again
-
-    mov si, hello_msg
-
-
- 
- ;loading kernel
-    mov bx, 0x8000
-    mov ah, 0x02
-    mov al, 10 
-    int 0x13
+   
+    cli ; stop interuping
+    xor ax,ax ; ax = 0
+    mov ds, ax
+    mov es ,ax
+    mov ss,ax 
+    mov sp,0x7C00 ;stack start from 0x7C00
     
-print_string:
-  lodsb
-  cmp al,0
-  je .done
-  mov ah, 0x0E
-  mov bh, 0x00
-  mov bl, 0x0D
-  int 0x10
-  jmp print_string
+    
+    mov si, hello_msg
+    call print_pm
 
-.done:
-    ret
 
-halt:
+ load_kerel:
+ ;loading kernel
+    mov ax, 0x1000
+    mov es,ax
+    
+    xor bx, bx ; bx = 0
+     
+    mov ah, 0x02 ; sectors function
+    mov al,10 ; reserve 10 sectors
+    mov ch, 0
+    mov cl, 2 ;sector 2 (1 for bootloader)
+    mov dh, 0
+    mov dl ,[BOOT_DRIVER]
+    
+    int 0x13
+    jc disk_error
 
-  cli
-  hlt 
-  jmp halt
-
+    mov si,welcome_pm
+    call print_pm
 
 switch_to_pm:
   cli 
   lgdt[Gdt_descriptor]
-  mov ebx, cr0
-  or ebx, 1 
-  mov cr0, ebx; close to protected mode
-
+  mov eax, cr0
+  or eax, 1 
+  mov cr0, eax; close to protected mode
+  
+  jmp CODE_SEG:init_pm
 ; creating GDT
+
+align 8
+
 Gdt_START:
     
     null_descriptor:
         dd 0
         dd 0 
     code_descriptor:
-        db 0xFFFF; limit 
+        dw 0xFFFF; limit 
         dw 0      ; base 16 bits +
         db 0      ; 8 bits = 24 bits
-        db 10011010; p,p,t,type flags
-        db 11001111; others + limits
+        db 10011010b; p,p,t,type flags
+        db 11001111b; others + limits
         db 0 ; lsst  8 bits of base 
      date_descriptor:
-        db 0xFFFF
+        dw 0xFFFF
         dw 0 
         db 0 
-        db 10010010; code = 0 => no
-        db 11001111
+        db 10010010b; code = 0 => no
+        db 11001111b
         db 0
 
 Gdt_END:
 
 Gdt_descriptor:
 
-   dw Gdt_END - Gdt_START -1 
-   dw Gdt_START
+   dw Gdt_END - Gdt_START - 1 
+   dd Gdt_START
 
-CODE_SEG equ code_descriptor - Gdt_START
-DATA_SEG equ date_descriptor - Gdt_START
+;CODE_SEG equ code_descriptor - Gdt_START
+;DATA_SEG equ date_descriptor - Gdt_START
 
-call switch_to_pm
 
-  jmp CODE_SEG:init_pm; far jump to init_pm
+; far jump to init_pm
 
 [bits 32]
 init_pm:
   
   mov ax, DATA_SEG
-  mov ds , ax
-  mov fs , ax
-  mov gs , ax
-  mov ss , ax
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  mov ss, ax
   
   mov ebp, 0x90000
   mov esp, ebp
 
-  mov ebx,welcome_pm
-  call print_pm
-  
-  jmp 0x8000
+  ;mov ebx,welcome_pm
+  ;call print_pm
+  mov byte [0xB8000] , 'L'
+  mov byte [0xB8001] , 0x4F
+  mov byte [0xB8002] , 'A'
+  mov byte [0xB8003] , 0x0F
 
+
+  jmp 0x10000
+
+[bits 16]
 print_pm:; helper function for 16 bits
   pusha
   mov ebx, 0x7C00
   .loop:
-      mov [ebx],al
-      mov ah , 0x0F
-      cmp al,0
+      lodsb
+      cmp al, 0
       je .done
-      mov [edx],ax
-      mov ebx ,1 
-      mov edx , 2
+      mov ah, 0x0E
+      mov bh , 0
+      int 0x10
       jmp .loop
 
   .done:
    popa 
    ret
 
+disk_error:
+  mov si, error_msg
+  call print_pm
+  jmp $
 
 
 ;strings      
