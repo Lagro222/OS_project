66a5cd8a0324394545806de655ca8d8eb9ab5677
Lagro222 <wbenkhaira@gmail.com>
2026-05-27 18:24:27 +0000

    change: blanking logic

--- src/kernel/screen/screen.c
+++ src/kernel/screen/screen.c
@@ -1,11 +1,11 @@
 #include "screen.h"
-char *vga = (char*)0xB8000 ;
 
+char *vga = (char*)0xB8000;
 
 void print(char *str){
   char *show_str = vga + ( 5 * 160);
     int i = 0;
-
+  
   while (str[i] != '\0') {
       show_str[i*2]  = str[i];
       show_str[i*2 + 1] = 0x0F;
@@ -15,16 +15,12 @@ void print(char *str){
 
 void clear_screen(){
   
-    for(int i = 0 ; i <= 24 ; i++ ){
-      for (int j = 0; j <= 79 ; j++) {
-        int k = i;
+    for(int i = 0 ; i < 24 ; i++ ){
+      for (int j = 0; j < 79 ; j++) {
+        
         char* vga_clr = vga + (i * 80 + j) * 2;
-        if (i % 2 == 0) {
-          vga_clr[i] = ' ';
-        }else {
-          vga_clr[i] = 0x00;
-        }
-
+        vga_clr[0] = ' ';
+        vga_clr[1] = 0x00;
       }
     }
 }
