0c999adb08c5d5e2718eb5a9f7ebb412dd794c16
Lagro222 <wbenkhaira@gmail.com>
2026-06-01 15:15:44 +0000

    change: using inb as static inline

--- src/kernel/hardware/ports.h
+++ src/kernel/hardware/ports.h
@@ -1,7 +1,12 @@
 #ifndef PORTS_H
 #define PORTS_H
 
-unsigned char inb( unsigned short port);
+static inline unsigned char inb(unsigned short port){ // ports 65335 in total
+    unsigned char result;// value from 0 to 255 
+ // put the port in the d register : dx and the and al take it , result takes what in the a register a : here we have al; 
+   asm volatile("inb %%dx, %%al" : "=a" (result) : "dN" (port) );  
+   return result;  
+}
 
 
 #endif // !PORTS_H
