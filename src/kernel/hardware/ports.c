a849daf0836e6a6921614bfa6a6db228d257158a
Lagro222 <wbenkhaira@gmail.com>
2026-06-01 15:15:44 +0000

    change: using inb as static inline

--- src/kernel/hardware/ports.c
+++ src/kernel/hardware/ports.c
@@ -1,9 +1,2 @@
 #include "ports.h"
 
-unsigned char inb(unsigned short port){ // ports 65335 in total
-    unsigned char result;
- // put the port in the d register : dx and the and al take it , result takes what in the a register a : here we have al; 
-   asm("in %%dx, %%al" : "=a" (result) : "d" (port) );  
-   return result;
-  
-}
