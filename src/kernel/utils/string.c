20b9b02120f7eef6da06aa64fc1e2994d482fcb0
Lagro222 <wbenkhaira@gmail.com>
2026-06-07 15:44:36 +0000

    add: counting lenght and delete the last element in string

--- src/kernel/utils/string.c
+++ src/kernel/utils/string.c
@@ -1,11 +1,29 @@
 #include "string.h"
 
-
-
 int mystrcmp(const char *s1, const char *s2){
   while (*s1 && *s2 && *s1 == *s2) {
       s1++;
       s2++;
   }
   return  *s1 - *s2;
+}
+
+int mystrlen(char *str){
+  int i = 0;
+  while(*str != '\0'){
+    i++;
+  }
+  return i;
+}
+
+char* rmlast(char *str){
+    
+  char *str2 = str;
+  int len = mystrlen(str2);
+  
+  if(len > 0){
+    str2[len - 1] = '\0';
+  }
+
+  return  str2;
 }
--- src/kernel/utils/string.h
+++ src/kernel/utils/string.h
@@ -2,6 +2,8 @@
 #define STRING_H
 
 int mystrcmp(const char *s1, const char *s2);
+int mystrlen(char *str);
+char* rmlast(char *str);
 
 
 #endif // !STRING_H
